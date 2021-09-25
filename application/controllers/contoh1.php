3624f1327f57fdb4f924b54e59496393e8522e20
nandasiw11 <ananda.pambayadii@gmail.com>
2021-09-25 12:37:21 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan </h1>";
+        echo "Nama saya Ananda Siwi Pambayadi
+            Saya tinggal didaerah Bojonggede Bogor
+            olahraga yang saya sukai adalah bersepeda";
+    }
+}
