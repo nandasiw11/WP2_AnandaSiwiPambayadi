aba992c425879560cf91cfe0287662bafeb008f5
nandasiw11 <ananda.pambayadii@gmail.com>
2021-10-01 14:38:48 +0700

    menampilkan latihan1

--- application/models/model_latihan1.php
+++ application/models/model_latihan1.php
@@ -4,6 +4,7 @@ class model_latihan1 extends CI_Model
     //membuat variable untuk menampung nilai
     public $nilai1, $nilai2, $hasil;
     
+    
     //method penjumlahan
     public function jumlah($n1 = null, $n2 = null)
     {
