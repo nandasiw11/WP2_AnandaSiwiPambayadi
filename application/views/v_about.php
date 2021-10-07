8bf45bfdab03db8f1c794165703076ea96b3c198
nandasiw11 <ananda.pambayadii@gmail.com>
2021-10-07 22:07:55 +0700

    membuat template sederhana & dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ananda</li>
+        <li>Nama Belakang : Siwi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl. Swadaya II no. 111 Bojonggede</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bersepeda</li>
+        <li>Membaca</li>
+    </ul>
